ba92d03e8018e49f215aebfdd6fd4fe782583316
Haaris Mian <mail@haarismian.com>
2018-06-13 23:35:42 -0400

    implement mongoose and mongo

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require("express");
-require("./services/passport");
+// Models
+require('./models/User');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-require("./routes/authRoutes")(app); // valid because authRoutes returns a function and app is the argument
+require('./routes/authRoutes')(app); // valid because authRoutes returns a function and app is the argument
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,29 @@
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("../config/keys");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token: ", accessToken);
-      console.log("refresh token: ", refreshToken);
-      console.log("profile: ", profile);
+      User.findOne({ googlId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          console.log('Existing user, id: ' + profile.id);
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+          console.log('new user saved, id: ' + profile.id);
+        }
+      });
     }
   )
 );
