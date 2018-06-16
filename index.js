162d8c16a64fb5cda024c1385a59d1a54779cafa
Haaris Mian <mail@haarismian.com>
2018-06-16 00:33:59 -0400

    added cookie management

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // Models
 require('./models/User');
 
+// Services
+require('./services/passport');
+
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-require('./services/passport');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app); // valid because authRoutes returns a function and app is the argument
 
