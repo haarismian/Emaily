dd36e90566adb8d0284ff32f7f620fec279a2111
Haaris Mian <mail@haarismian.com>
2018-06-19 01:18:13 -0400

    seperated dev vs prod keys

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+// dev.js don't commit this
+
+module.exports = {
+  googleClientID:
+    '195921937422-rroe5ovviqkv4skank78e1mjippvld8v.apps.googleusercontent.com',
+  googleClientSecret: 'FUWKfUM6lkpy9gWTTMfKiqJR',
+  mongoURI:
+    'mongodb://haarismian:haarisM123@ds253840.mlab.com:53840/dev-emaily',
+  cookieKey: 'bsflakbjkdls23asd!@Egf'
+};
